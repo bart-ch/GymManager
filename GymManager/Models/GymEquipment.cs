988f7e2a4f8f6bfa216784ea83eb14feb1269abb
Bartosz Chodyła <bartoszchodyla@gmail.com>
2020-10-30 19:31:56 +0100

    Add GymEquipment model

---
+++ GymManager/Models/GymEquipment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GymManager.Models
+{
+    public class GymEquipment
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string TypeOfEquipment { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Brand { get; set; }        
+        
+        [Required]
+        [StringLength(255)]
+        public string Model { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+    }
+}
