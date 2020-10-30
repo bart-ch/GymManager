dabe2cbcba4415fa88b26cc55979572890dd11f5
Bartosz Chodyła <bartoszchodyla@gmail.com>
2020-10-30 19:00:10 +0100

    Initial commit

---
+++ GymManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymManager.Startup))]
+namespace GymManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
