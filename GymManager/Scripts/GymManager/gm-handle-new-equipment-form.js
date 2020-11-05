587df774f42d19f3e0d74e0da9a5132f102292ab
Bartosz Chodyła <bartoszchodyla@gmail.com>
2020-11-05 21:38:33 +0100

    Fixed new equipment JS file

--- GymManager/Scripts/GymManager/gm-handle-new-equipment-form.js
+++ GymManager/Scripts/GymManager/gm-handle-new-equipment-form.js
@@ -3,29 +3,10 @@
     getResourcesFromAPIAndInsertInSelect("#areaId", "Gym Area", "areas");
     getResourcesFromAPIAndInsertInSelect("#typeId", "Equipment Type", "types");
 
-    var url = $(location).attr('href');
-    var id = url.substring(url.lastIndexOf('/') + 1);
-    if (id > 0) {
-        $.ajax({
-            type: "GET",
-            url: "/api/equipment/" + id,
-        })
-            .done(function (equipment) {
-                $("#brand").val(equipment.brand);
-                $("#model").val(equipment.model);
-                $("#typeId").val(equipment.type.id);
-                $("#areaId").val(equipment.area.id);
-                var date = new Date(equipment.purchaseDate);
-                var dateString = date.getFullYear() + '-'
-                    + ('0' + (date.getMonth() + 1)).slice(-2) + '-'
-                    + ('0' + date.getDate()).slice(-2);
-                $("#purchaseDate").val(dateString);
-
-            })
-            .fail(function () {
-                toastr.error("Sorry, the equipment doesnt exist");
-            });
-    }
+    function resetForm($form) {
+        $form.find('input:text, input:password, input:file, select, textarea').val('');
+        $("input[type=date]").val("");
+    };
 
     $("#equipmentForm").validate({
         errorPlacement: function ($error, $element) {
@@ -34,10 +15,10 @@
         },
         rules: {
             brand: {
-                maxlength: 30
+                maxlength: 30,
             },
             model: {
-                maxlength: 30
+                maxlength: 30,
             },
             serialNumber: {
                 maxlength: 30
@@ -49,12 +30,15 @@
                 return obj;
             }, {});
             $.ajax({
-                url: "/api/equipment/" + id,
-                method: "PUT",
+
+                url: "/api/equipment",
+                method: "POST",
                 data: formData
             })
                 .done(function () {
-                    toastr.success("Equipment successfully updated.");
+                    toastr.success("Equipment successfully added.");
+                    resetForm($('#equipmentForm'));
+
                 })
                 .fail(function () {
                     toastr.error("Unexpected error.");
