ff30a221086b433f262106afe3087fd6626e79c0
Bartosz Chodyła <bartoszchodyla@gmail.com>
2020-11-26 00:35:48 +0100

    Edit default register and manage views. Also add profile data

--- GymManager/Controllers/AccountController.cs
+++ GymManager/Controllers/AccountController.cs
@@ -149,7 +149,14 @@ namespace GymManager.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser 
+                { 
+                    UserName = model.Email, 
+                    Email = model.Email,
+                    Name = model.Name,
+                    Surname = model.Surname,
+                    JobTitle = model.JobTitle
+                };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- GymManager/Core/Domain/ApplicationUser.cs
+++ GymManager/Core/Domain/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -8,6 +9,18 @@ namespace GymManager.Core.Domain
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string Surname { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string JobTitle { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
