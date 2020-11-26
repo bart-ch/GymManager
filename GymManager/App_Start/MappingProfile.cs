8490fb32ed8bfc9022b89b5315d06380369931d4
Bartosz Chodyła <bartoszchodyla@gmail.com>
2020-11-26 14:54:19 +0100

    Refactor mapping profile by using ReverseMap()

--- GymManager/App_Start/MappingProfile.cs
+++ GymManager/App_Start/MappingProfile.cs
@@ -9,34 +9,26 @@ namespace GymManager.App_Start
         public MappingProfile()
         {
             Mapper.CreateMap<Equipment, EquipmentDto>();
-            Mapper.CreateMap<Area, AreaDto>();
-            Mapper.CreateMap<Core.Domain.Type, TypeDto>();
+            Mapper.CreateMap<Area, AreaDto>().ReverseMap();
+            Mapper.CreateMap<Core.Domain.Type, TypeDto>().ReverseMap();
             Mapper.CreateMap<Supplement, SupplementDto>();
             Mapper.CreateMap<Flavor, FlavorDto>();
-            Mapper.CreateMap<SupplementType, SupplementTypeDto>();
+            Mapper.CreateMap<SupplementType, SupplementTypeDto>().ReverseMap();
             Mapper.CreateMap<Malfunction, MalfunctionDto>();
             Mapper.CreateMap<EquipmentOrder, EquipmentOrderDto>();
-            Mapper.CreateMap<OrderStatus, OrderStatusDto>();
-            Mapper.CreateMap<ApplicationUser, ApplicationUserDto>()
-                .ReverseMap();
+            Mapper.CreateMap<OrderStatus, OrderStatusDto>().ReverseMap();
+            Mapper.CreateMap<ApplicationUser, ApplicationUserDto>().ReverseMap();
 
             Mapper.CreateMap<EquipmentDto, Equipment>()
                 .ForMember(e => e.Id, opt => opt.Ignore());
-            Mapper.CreateMap<AreaDto, Area>();
-            Mapper.CreateMap<TypeDto, Core.Domain.Type>();
             Mapper.CreateMap<SupplementDto, Supplement>()
                 .ForMember(s => s.Id, opt => opt.Ignore());
             Mapper.CreateMap<FlavorDto, Flavor>()
                 .ForMember(f => f.Id, opt => opt.Ignore());
-            Mapper.CreateMap<SupplementTypeDto, SupplementType>();
             Mapper.CreateMap<MalfunctionDto, Malfunction>()
-                .ForMember(m => m.Id, opt => opt.Ignore());           
+                .ForMember(m => m.Id, opt => opt.Ignore());
             Mapper.CreateMap<EquipmentOrderDto, EquipmentOrder>()
                 .ForMember(m => m.Id, opt => opt.Ignore());
-            Mapper.CreateMap<OrderStatusDto, OrderStatus>();
-            Mapper.CreateMap<ApplicationUserDto, ApplicationUser>()
-                    .ReverseMap();
-
         }
     }
 }
