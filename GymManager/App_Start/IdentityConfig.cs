14d46635aa842101c01b896f42dddb3a27a733a9
Bartosz Chodyła <bartoszchodyla@gmail.com>
2020-12-01 22:33:45 +0100

    Add client-site password verifictation

--- GymManager/App_Start/IdentityConfig.cs
+++ GymManager/App_Start/IdentityConfig.cs
@@ -51,7 +51,7 @@ namespace GymManager
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
--- GymManager/ViewModels/AccountViewModels.cs
+++ GymManager/ViewModels/AccountViewModels.cs
@@ -83,7 +83,7 @@ namespace GymManager.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$", ErrorMessage = "Password must contain minimum 6 characters, at least one lower- and uppercase and a number.")]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
